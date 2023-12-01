25a37f921c560ff6f17e0dd7b994398f605efb42
aHasnat1997 <hasnat56703@gmail.com>
2023-12-02 00:01:52 +0600

    FIX: AppRoutes and ADD: UserRoutes

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from "express"
-import { UserController } from "../controllers/user.controller"
+import express, { Router } from "express"
+import { UserRoutes } from "./user.route";
 
 // creating user route 
 export const AppRoutes = express.Router();
@@ -8,45 +8,14 @@ export const AppRoutes = express.Router();
 // App route array
 interface TRoute {
     path: string,
-    controller: (req: Request, res: Response) => Promise<void>
+    route: Router
 }
 const routes: TRoute[] = [
     {
         path: '/users',
-        controller: UserController.createUser
-    },
-    {
-        path: '/users',
-        controller: UserController.getAllUser
-    },
-    {
-        path: '/users/:id',
-        controller: UserController.getSingleUserId
-    },
-    {
-        path: '/users/email/:email',
-        controller: UserController.getSingleUserEmail
-    },
-    {
-        path: '/users/:id',
-        controller: UserController.updateSingleUser
+        route: UserRoutes
     }
 ];
 
 
-routes.forEach(route => AppRoutes.use(route.path, route.controller));
-
-// POST route for creating user in DB
-// AppRoutes.post('/', UserController.createUser);
-
-// GET route for all user from DB
-// AppRoutes.get('/', UserController.getAllUser);
-
-// GET route for single user from DB using id
-// AppRoutes.get('/:id', UserController.getSingleUserId);
-
-// GET route for single user from DB using email
-// AppRoutes.get('/email/:email', UserController.getSingleUserEmail);
-
-// PATCH route for update single user in DB
-// AppRoutes.patch('/:id', UserController.updateSingleUser);
+routes.forEach(route => AppRoutes.use(route.path, route.route));
