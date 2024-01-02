0c6510e0399394575411f1bf8adf2e9574163d1c
aHasnat1997 <hasnat56703@gmail.com>
2024-01-02 22:03:25 +0600

    ADD: cookie-parser

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import path from 'path';
 import { AppRoutes } from './route';
+import cookieParser from 'cookie-parser';
 
 // cerate an app using express
 const app: Application = express();
@@ -9,6 +10,7 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 // api routes
 app.use('/api/v1', AppRoutes);
